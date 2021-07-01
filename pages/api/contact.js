454151be4dd7d9465a4acb3b689bf5ab2042faed
Blake DeGraw <bbdegraw@gmail.com>
2021-06-30 19:07:44 -0700

    test email sent successfully

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,28 @@
 export default function (req, res){
-    console.log(req.body)
+    require('dotenv').config()
+    let nodemailer = require('nodemailer')
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            type: 'login',
+            user: 'blakedegrawbeats@gmail.com',
+            pass: process.env.password
+        },
+        secure: true,
+    })
+    const mailData = {
+        from: 'blakedegrawbeats@gmail.com',
+        to: 'bbdegraw@gmail.com',
+        subject: `Message from ${req.body.name}`,
+        text: req.body.message + ' | sent from: ' + req.body.email,
+        html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`
+    }
+    transporter.sendMail(mailData, function (err, info) {
+        if(err){
+            console.log(err)
+        } else{
+            console.log(info)
+        }
+    })
+    res.status(200)
 }
